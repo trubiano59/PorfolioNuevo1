845950ff2f5ee000a2c80aaabc6644c30943c6fc
Miguel Trubiano <trubiano59@hotmail.com>
2022-10-17 01:34:43 -0300

    arreglos boton

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  mensaje!: string;
+  form: any = {
+    email: null,
+    password: null,
+  };
 
   constructor() { }
 
