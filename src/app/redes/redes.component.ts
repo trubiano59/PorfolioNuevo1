f9b640e2c5dbd264263efbaac1b7ad89b9c0bda2
Miguel Trubiano <trubiano59@hotmail.com>
2022-10-19 14:06:25 -0300

    Arreglo de pasar al cerrar del dashboard al index

--- src/app/redes/redes.component.ts
+++ src/app/redes/redes.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-redes',
   templateUrl: './redes.component.html',
   styleUrls: ['./redes.component.css']
 })
 export class RedesComponent implements OnInit {
+
 
   constructor() { }
 
